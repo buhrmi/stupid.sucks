59deba725479ea0a7536596cc4d655456ba002c3
Stefan Buhrmester <buhrmi@gmail.com>
2016-01-04 20:15:44 +0900

    signing up

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: prices
+#
+#  id               :integer          not null, primary key
+#  amount           :integer
+#  currency         :string
+#  purchasable_id   :integer
+#  purchasable_type :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Price < ActiveRecord::Base
   belongs_to :purchasable, :polymorphic => true
 end
--- test/models/price_test.rb
+++ test/models/price_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: prices
+#
+#  id               :integer          not null, primary key
+#  amount           :integer
+#  currency         :string
+#  purchasable_id   :integer
+#  purchasable_type :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 require 'test_helper'
 
 class PriceTest < ActiveSupport::TestCase
